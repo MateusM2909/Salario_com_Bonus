d2fa043fc8779c30bb9939c7c501a9ffe6bd40bc
Mateus Marinho <massis.mateus@gmail.com>
2023-03-12 20:54:33 -0300

    first commit

---
+++ atv3AED1203/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+
+namespace atv3AED1203
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            cliente c = new cliente();
+            c.Nome = "Manoel Gomez";
+            c.SalarioFixo = 500.00;
+            c.Vendas = 1230.30;
+
+            c.ImprimirDados();
+            c.total();
+
+        }
+    }
+}
---
+++ atv3AED1203/cliente.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace atv3AED1203
+{
+    internal class cliente
+    {
+        private string nome;
+        private double salarioFixo;
+        private double vendas;
+
+        public string Nome
+        {
+            get{return nome;}
+            set{nome = value;}
+        }
+        public double SalarioFixo
+        {
+            get { return salarioFixo;}
+            set { salarioFixo = value;}
+        }
+        public double Vendas
+        {
+            get { return vendas;}
+            set { vendas = value;}
+        }
+
+        public void ImprimirDados()
+        {
+            Console.WriteLine("Nome: " + nome);
+            Console.WriteLine("Salario Fixo: " + salarioFixo.ToString("F2"));
+            Console.WriteLine("Montante de vendas: " + vendas);
+            Console.WriteLine();
+            Console.WriteLine();
+        }
+
+        public void total()
+        {
+            double x = (vendas * 0.15) + salarioFixo;
+            Console.WriteLine("Nome: " + nome);
+            Console.WriteLine("Salario Final: " + x.ToString("F2"));
+        }
+
+    }
+}
